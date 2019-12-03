e43d0fb9f9f090292d4c2ee4004514361405d207
thanseeh <thanseehabdulla@gmail.com>
2019-12-03 10:06:38 +0530

    initial

---
+++ app/src/main/java/com/example/capturenow/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.capturenow;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.MediaStore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
+
+
+    }
+
+}
